6cb21a70da31cdba2c483ec0a97772b64c38eeaf
DAVID <DAVID@DAVID-PC>
2015-06-14 17:16:12 +0200

    poniendo botones

--- examenjuny14/src/ventanas/VentanaPrincipal.java
+++ examenjuny14/src/ventanas/VentanaPrincipal.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -32,11 +33,22 @@ public class VentanaPrincipal extends JFrame {
 	 */
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 385);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton consultaImpaButton = new JButton("Consultar Impagaments");
+		consultaImpaButton.setBounds(137, 88, 157, 23);
+		contentPane.add(consultaImpaButton);
+		
+		JButton actualizaImpaButton = new JButton("Actualitzar Impagaments");
+		actualizaImpaButton.setBounds(126, 164, 190, 23);
+		contentPane.add(actualizaImpaButton);
+		
+		JButton buscadatosSocioButton = new JButton("Cercar dades soci");
+		buscadatosSocioButton.setBounds(162, 233, 132, 23);
+		contentPane.add(buscadatosSocioButton);
 	}
-
 }
